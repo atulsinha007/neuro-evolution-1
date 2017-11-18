d37821b47e679622d0b4e600530121fce00ead7a
Raghav Khandelwal <raghavthecyberdude@gmail.com>
2017-11-18 23:57:54 +0530

    Little cleanup

--- postmidsem/codes/pilot/main.py
+++ postmidsem/codes/pilot/main.py
@@ -21,7 +21,7 @@ creator.create("Individual", Chromosome, fitness=creator.FitnessMin)
 toolbox = base.Toolbox()
 
 def minimize(individual):
-	network_obj = Neterr(indim, outdim, n_hidden, np.random)
+	network_obj = Neterr(indim, outdim, n_hidden, numpy.random)
 	outputarr = network_obj.feedforward_ne(individual)
 
 	neg_log_likelihood_val = objectives.give_neg_log_likelihood(outputarr, network_obj.resty)
@@ -31,23 +31,18 @@ def minimize(individual):
 
 	return neg_log_likelihood_val, mean_square_error_val, false_positve_rat, false_negative_rat
 
-
 def mycross(ind1, ind2, gen_no):
-	child1 = crossover(ind1, ind2, gen_no, inputdim=8, outputdim=1)
-	child2 = crossover(ind1, ind2, gen_no, inputdim=8, outputdim=1)
-
+	child1 = crossover(ind1, ind2, gen_no, indim, outdim)
+	child2 = crossover(ind1, ind2, gen_no, indim, outdim)
 	return child1, child2
-
 
 def mymutate(ind1):
 	new_ind = ind1.do_mutation(0.2, 0.1, 0.05, indim, outdim, n_hidden, numpy.random)
 	return new_ind
 
-
 def initIndividual(ind_class, inputdim, outputdim):
 	ind = ind_class(inputdim, outputdim)
 	return ind
-
 
 toolbox.register("individual", initIndividual, creator.Individual, indim, outdim)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
@@ -59,8 +54,8 @@ toolbox.register("select", tools.selNSGA2)
 
 def main(seed=None):
 	random.seed(seed)
-	NGEN = 2
-	MU = 4 * 10  # this has to be a multiple of 4. period.
+	NGEN = 250
+	MU = 4 * 25  # this has to be a multiple of 4. period.
 	CXPB = 0.9
 
 	stats = tools.Statistics(lambda ind: ind.fitness.values[1])
@@ -87,17 +82,13 @@ def main(seed=None):
 	print(logbook.stream)
 	maxi = 0
 	stri = ''
-	# Begin the generational process
-	# print(pop.__dir__())
-	for gen in range(1, NGEN):
 
+	# Begin the generational process
+	for gen in range(1, NGEN):
 		# Vary the population
 		offspring = tools.selTournamentDCD(pop, len(pop))
 		offspring = [toolbox.clone(ind) for ind in offspring]
 
-		# print("changed?", gen)
-		# print(maxi)
-		# print("length",len(offspring))
 		for ind1, ind2 in zip(offspring[::2], offspring[1::2]):
 			# print(ind1.fitness.values)
 			if random.random() <= CXPB:
@@ -135,24 +126,25 @@ if __name__ == "__main__":
 	pop, stats = main()
 
 	fronts = tools.sortNondominated(pop, len(pop))
+
 	if len(fronts[0]) < 30:
 		pareto_front = fronts[0]
 	else:
-
 		pareto_front =  random.sample(fronts[0], 30)
+	
 	print("Pareto Front: ")
+	
 	for i in range(len(pareto_front)):
 		print(pareto_front[i].fitness.values)
 
-	neter = Neterr(indim, outdim, n_hidden, np.random)
+	neter = Neterr(indim, outdim, n_hidden, numpy.random)
 	file_ob = open("log.txt", "a")
 
 	print("\ntest: test on one with min validation error", neter.test_err(min(pop, key=lambda x: x.fitness.values[1])))
 	tup = neter.test_on_pareto_patch(pareto_front)
 
 	print("\n test: avg on sampled pareto set", tup[0], "least found avg", tup[1])
-	file_ob.write(
-		"test on one with min validation error " + str(neter.test_err(min(pop, key=lambda x: x.fitness.values[1]))))
+	file_ob.write("test on one with min validation error " + str(neter.test_err(min(pop, key=lambda x: x.fitness.values[1]))))
 	file_ob.write("\ntest: avg on sampled pareto set " + str(tup[0]) + " least found avg " +
 				  str(tup[1]))
 	file_ob.close()
