f3f7ec054db98cbc70d564a1db2eb3b1613e054e
Atul Sinha <sinhaa297@gmail.com>
2018-02-01 10:43:04 +0530

    Dataset upload and papershredder, breadmaker added

--- postmidsem/codes/main_folder/pilot/data_pickler.py
+++ postmidsem/codes/main_folder/pilot/data_pickler.py
@@ -11,8 +11,8 @@ from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 pstri = './'
 fstri = '/home/placements2018/Dataset3/'
-dir_lis_src = ['010.beer-mug', '043.coin', '004.baseball-bat']
-dir_lis_tar = ['041.coffee-mug', '033.cd', '234.tweezer']
+dir_lis_src = ['010.beer-mug', '043.coin', '004.baseball-bat', '156.paper-shredder']
+dir_lis_tar = ['041.coffee-mug', '033.cd', '234.tweezer', '021.breadmaker']
 ctr = 0
 def files(path):
     for file in os.listdir(path):
